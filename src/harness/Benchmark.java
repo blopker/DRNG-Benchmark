7188456cf23528352610c5517ed73db5097b383e
ninj0x <ninj0x@gmail.com>
2012-12-09 00:16:49 -0800

    print sorted results

--- src/harness/Benchmark.java
+++ src/harness/Benchmark.java
@@ -1,10 +1,14 @@
 package harness;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.reflections.Reflections;
 
@@ -12,36 +16,48 @@ import drng.DRNG;
 
 public class Benchmark {
 	private static Map<DRNG, Long> results = new HashMap<DRNG, Long>();
-	
+
 	public static void main(String[] args) {
 		Reflections ref = new Reflections("drng");
 		Set<Class<? extends DRNG>> drngClasses = ref.getSubTypesOf(DRNG.class);
-		
 		List<DRNG> drngs = createDRNGs(drngClasses);
-		
+
 		runTests(drngs);
-		
+
 		System.out.println("Tests done!");
+		printResults(results);
 	}
 
+	/**
+	 * Runs each DRNG for runTime milliseconds. Then stores results in result
+	 * map.
+	 * 
+	 * @param drngs
+	 */
 	private static void runTests(List<DRNG> drngs) {
 		for (DRNG drng : drngs) {
 			System.out.println("Running test: " + drng.getClass().getName());
-			
+
+			// Set the seed as a control.
+			drng.setSeed(0);
+
 			long count = 0;
 			int runTime = 3000;
 			long start = System.currentTimeMillis();
 			long end = start + runTime;
-			
-			while (System.currentTimeMillis() < end){
+
+			while (System.currentTimeMillis() < end) {
 				drng.run();
 				count++;
 			}
+			// Get the actual duration in case the while loop ran over.
 			long duration = System.currentTimeMillis() - start;
-			
-			long speed = count/(duration/1000);
+
+			long speed = count / (duration / 1000);
 			results.put(drng, speed);
-			System.out.println(drng.getClass().getName() + " done. Speed: " + speed + " num/sec");
+
+			System.out.println(drng.getClass().getSimpleName()
+					+ " done. Speed: " + speed + " num/sec");
 		}
 	}
 
@@ -56,7 +72,28 @@ public class Benchmark {
 		}
 		return drngs;
 	}
-	
-	
-	
+
+	private static void printResults(Map<DRNG, Long> res) {
+		SortedSet<Map.Entry<DRNG, Long>> entries = entriesSortedByValues(res);
+
+		System.out.println("Results (higher is better):");
+		for (Entry<DRNG, Long> entry : entries) {
+			String drngName = entry.getKey().getClass().getSimpleName();
+			System.out.println(entry.getValue() + " num/sec -> " + drngName);
+		}
+	}
+
+	private static <K, V extends Comparable<? super V>> SortedSet<Map.Entry<K, V>> entriesSortedByValues(
+			Map<K, V> map) {
+		SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<Map.Entry<K, V>>(
+				new Comparator<Map.Entry<K, V>>() {
+					@Override
+					public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+						return e1.getValue().compareTo(e2.getValue());
+					}
+				});
+		sortedEntries.addAll(map.entrySet());
+		return sortedEntries;
+	}
+
 }
